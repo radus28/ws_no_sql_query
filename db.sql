b6dd2f019fcdab2e9c171915136e2dd89663141b
ajstharsan-radus28 <ajstharsan@gmail.com>
2016-12-06 14:34:51 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,6 @@
+
+UPDATE `vtiger_ws_operation_seq` SET `id` = (id+1);
+INSERT INTO `vtiger_ws_operation` (`operationid`, `name`, `handler_path`, `handler_method`, `type`, `prelogin`) VALUES ((SELECT id FROM `vtiger_ws_operation_seq`), 'retrievequery', 'include/Webservices/RetrieveQuery.php', 'vtws_retrievequery', 'GET', '0');
+
+INSERT INTO `vtiger_ws_operation_parameters` (`operationid`, `name`, `type`, `sequence`) VALUES 
+((SELECT id FROM `vtiger_ws_operation_seq`), 'module', 'String', '1'),((SELECT id FROM `vtiger_ws_operation_seq`), 'andWhere', 'encoded', '2'),((SELECT id FROM `vtiger_ws_operation_seq`), 'orWhere', 'encoded', '3'); 
